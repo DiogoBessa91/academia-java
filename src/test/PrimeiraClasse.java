878d2652cd18427521b6e2952a9bd6e9898c454b
DiogoBessa <diogobessac@gmail.com>
2019-06-25 11:30:13 +0100

    terça manhã

---
+++ src/test/PrimeiraClasse.java
@@ -0,0 +1,27 @@
+package test;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+	
+		Boolean boolean1 = new Boolean (true);
+	 		
+		System.out.println(boolean1);
+		System.out.println(" ");
+
+		int num = 20;
+		Integer obj=Integer.valueOf(num);
+
+		System.out.println(num+" "+obj);
+		System.out.println(" ");
+		
+		Integer obj1 = new Integer(50);
+		int num1 = obj1.intValue();
+		System.out.println(num1+" "+obj1);
+		
+		
+		
+		}
+		
+};
+
